dd4ab5e7f17e817361bce4a0f6c7e8d0491e4e53
Lucija Sutlić <lsutlic@veleri.hr>
2025-11-27 12:56:13 +0100

    push

---
+++ frontend/src/pages/UnosPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md q-gutter-sm">
+    <q-btn color="white" text-color="black" label="Standard" />
+    
+  </div>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
